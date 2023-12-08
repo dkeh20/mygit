052ab0b3ca8eea8f6753902814fa70858b65032d
dkeh20 <dkeh20@naver.com>
2023-12-08 15:25:26 +0900

    first commit

---
+++ src/login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <table border="1">
+        <tr>
+            <td>
+                <input />
+                    <button>로그인</button>
+            </td>
+        </tr>
+    </table>
+  )
+}
+
+export default Login;
